140f084298de44ce8ea6b2a69441a76a948004d5
garciacallejas <david.garcia.callejas@gmail.com>
2021-01-09 17:18:58 +0100

    .

---
+++ packages.R
@@ -0,0 +1,4 @@
+
+install.packages(c("tidyverse","rmarkdown","knitr","here","broom",
+                 "correlation","corrplot","moments","visreg","effects",
+                 "lme4","DHARMa"),dependencies = TRUE)
